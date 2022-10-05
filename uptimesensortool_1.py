990f201e1c88c870b41fabf1eafb53a006f441d9
Zachary Derrick <derrick.zachary@gmail.com>
2022-10-05 02:45:44 +0000

    a python script that ping tests if a site is up

---
+++ uptimesensortool_1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Script: Ops 401 Class 02 Uptime Sensor Tool Part 1 of 2
+# Author: Zachary Derrick                    
+# Date of latest revision:  10/4/22    
+# Purpose: Write an uptime sensor tool that checks systems are responding. 
+
+# import libraries
+
+import time
+import datetime
+import os
+# import sys
+
+# declares variables 
+
+timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+# result = []
+
+# declare functions
+def updog():
+    hostname = "8.8.8.8"
+    status = os.system("ping -c 1 " + hostname)  
+    if status == 0:
+            print("host 8.8.8.8 is up", timestamp)
+            # result.append(timestamp)
+            # print(result)
+            # print("host 8.8.8.8 is up")
+    else: 
+            print("host 8.8.8.8 is down", timestamp)
+            
+    
+# print(result)
+
+while True:
+    updog()
+    time.sleep(2)
+# end
