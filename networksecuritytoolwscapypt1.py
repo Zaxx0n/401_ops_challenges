c2be808b28683b8db3b6d2fb65aeb9985023b805
Zachary Derrick <derrick.zachary@gmail.com>
2022-10-19 04:56:26 +0000

    Putting together the menu system and more for pt 2, still more work till functioning

--- networksecuritytoolwscapypt1.py
+++ networksecuritytoolwscapypt1.py
@@ -24,37 +24,3 @@ for port in sport:
     else:
         print(str(port) +" is filtered and silently dropped")
 
-# p=sr1(IP(dst=host)/TCP(sport))
-# if p:
-#     p.show()
-
-
-# def hostIP ():
-#     input("Enter a Host IP: ")
-#     host = hostIP()
-
-# def portrange ():
-#     input("Enter the port(s) to scan: ")
-#     sport = portrange
-
-# def main_menu():
-#     choice ='0'
-#     print("\n::::::::Scapy Services::::::::")
-#     print("Choose 1 to Enter Host IP")
-#     print("Choose 2 to Set Range of Ports")
-# #     print("Choose 3 to Scan Port Range")
-#     print("Choose 0 to Quit Program")
-#     choice = input("Please make a choice: ")
-#     if choice == "1":
-#         hostIP()
-#     elif choice == "2":
-#         portrange()
-#     elif choice == "0":
-#         sys.exit("\nIf you want to keep a secret, you must also hide it from yourself.")
-#     else:
-#         print("\nThat is not an option.")
-    
-# # # main 
-
-# while True:
-#     main_menu()
