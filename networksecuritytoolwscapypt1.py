4e9ddf702a92de69ba1b19617d75b29759741667
Zachary Derrick <derrick.zachary@gmail.com>
2022-10-20 01:39:26 +0000

    Reformatted and cleaned up the script a bit.

--- networksecuritytoolwscapypt1.py
+++ networksecuritytoolwscapypt1.py
@@ -9,14 +9,13 @@ import random
 from sys import flags
 from scapy.all import sr1, sr, IP, ICMP, TCP
 
-host = "10.0.0.181"
+host = "10.0.0.80" 
 # sport, scan port range...yeah, sport
 sport = (20, 21, 22, 80)
 src_port = 22
 for port in sport:
     response=sr1(IP(dst=host)/TCP(sport=src_port,dport=port,flags="S"),timeout=1,verbose=0,) 
-    if response is not None and response.haslayer(TCP):
-        print(str(port)) +": "
+
     if response is not None and response.haslayer(TCP) and response.getlayer(TCP).flags==0x12:
         print("Hey," + str(sport) +": is open")
     if response is not None and response.haslayer(TCP) and response.getlayer(TCP).flags==0x14:
